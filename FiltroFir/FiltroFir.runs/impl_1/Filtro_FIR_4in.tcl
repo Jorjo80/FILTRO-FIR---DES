a44e957872ba8ef1db9a2c7cc6dec44e9e5300a8
Jorjo80 <j_contri@hotmail.com>
2019-04-24 19:11:44 +0200

    12

--- FiltroFir/FiltroFir.runs/impl_1/Filtro_FIR_4in.tcl
+++ FiltroFir/FiltroFir.runs/impl_1/Filtro_FIR_4in.tcl
@@ -75,7 +75,6 @@ set rc [catch {
   set_property ip_cache_permissions {read write} [current_project]
   add_files -quiet H:/Jorge/UPM/master/1erCuatri/DES/Trabajo_filtroFIR/FILTRO-FIR---DES/FiltroFir/FiltroFir.runs/synth_1/Filtro_FIR_4in.dcp
   link_design -top Filtro_FIR_4in -part xc7z020clg400-1
-  write_hwdef -force -file Filtro_FIR_4in.hwdef
   close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
