e7ea79776cc10879c6aeb2c8d65d709bf08ac7c4
Jorjo80 <j_contri@hotmail.com>
2019-07-12 16:04:57 +0200

    fg

---
+++ 2017.1/FiltroFir/project_1/project_1.sdk/FirBareMetal/Debug/src/helloworld.d
@@ -0,0 +1,18 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../FirBareMetal_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FirBareMetal_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FirBareMetal_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FirBareMetal_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../FirBareMetal_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FirBareMetal_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FirBareMetal_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FirBareMetal_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
